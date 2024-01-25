9d8f8d66dc5fb82f7c359511ea2451a1333df062
GianmarcoDandrea <gianm96@hotmail.it>
2024-01-25 09:38:23 +0100

    update: package.json updated

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,7 @@ import "./bootstrap";
 
 import "~resources/scss/app.scss";
 // Per permettere a vite di processare le immagini
-import.meta.glob("../img/**");
+import.meta.glob(["../img/**"]);
 
 // Importiamo parte js di bootstrap css
 import * as bootstrap from "bootstrap";
